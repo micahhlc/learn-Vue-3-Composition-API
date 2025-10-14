d6413a8a19c354bc92211283d04024e6df65a9ce
Micah Cheng <micahhlc@gmail.com>
2025-10-14 21:09:10 +0900

    update L4-start

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,26 @@
 <script setup>
-import { ref } from 'vue'
-import socksGreenImage from './assets/images/socks_green.jpeg'
+  import { ref } from 'vue';
+  import socksGreenImage from './assets/images/socks_green.jpeg';
 
-const product = ref('Socks')
-const image = ref(socksGreenImage)
-
+  const product = ref('Socks');
+  const image = ref(socksGreenImage);
+  const inventory = ref(0);
+  const onSale = ref(false);
 </script>
-  
+
 <template>
   <div class="nav-bar"></div>
   <div class="product-display">
     <div class="product-container">
-      <div class="product-image">    
-        <img v-bind:src="image">
+      <div class="product-image">
+        <img v-bind:src="image" />
       </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
+        <p v-if="inventory > 10">In Stock</p>
+        <p v-else-if="inventory <= 10 && inventory > 0">Almost sold out!</p>
+        <p v-else>Out of Stock</p>
+        <p v-show="onSale">On Sale</p>
       </div>
     </div>
   </div>
