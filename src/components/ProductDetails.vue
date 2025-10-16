a5993d6ddfab87227d3a89be7b91c1b628bda623
Micah Cheng <micahhlc@gmail.com>
2025-10-17 00:07:43 +0900

    update L9

---
+++ src/components/ProductDetails.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import { ref } from 'vue';
+
+  const props = defineProps({
+    details: {
+      type: Array,
+      required: false,
+    },
+  });
+</script>
+<template>
+  <ul>
+    <li v-for="detail in props.details">{{ detail }}</li>
+  </ul>
+</template>
