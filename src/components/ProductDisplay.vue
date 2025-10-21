70436f15451ee95f4f43816285f6d0814c058070
Micah Cheng <micahhlc@gmail.com>
2025-10-22 00:36:10 +0900

    add and update productdisplay.vue reviewform.vue reviewlist.vue

--- src/components/ProductDisplay.vue
+++ src/components/ProductDisplay.vue
@@ -1,64 +1,74 @@
 <script setup>
-import { ref, computed } from 'vue'
-import socksGreenImage from '@/assets/images/socks_green.jpeg'
-import socksBlueImage from '@/assets/images/socks_blue.jpeg'
+  import { ref, computed } from 'vue';
+  import socksGreenImage from '@/assets/images/socks_green.jpeg';
+  import socksBlueImage from '@/assets/images/socks_blue.jpeg';
+  import ReviewForm from '@/components/ReviewForm.vue';
+  import ReviewList from '@/components/ReviewList.vue';
 
-const props = defineProps({
-  premium: {
-    type: Boolean,
-    required: true
-  }
-})
+  const props = defineProps({
+    premium: {
+      type: Boolean,
+      required: true,
+    },
+  });
 
-const emit = defineEmits(['add-to-cart'])
+  const emit = defineEmits(['add-to-cart']);
 
-const product = ref('Socks')
-const brand = ref('Vue Mastery')
+  const product = ref('Socks');
+  const brand = ref('Vue Mastery');
 
-const selectedVariant = ref(0)
-  
-const details = ref(['50% cotton', '30% wool', '20% polyester'])
+  const selectedVariant = ref(0);
 
-const variants = ref([
-  { id: 2234, color: 'green', image: socksGreenImage, quantity: 50 },
-  { id: 2235, color: 'blue', image: socksBlueImage, quantity: 0 },
-])
+  const details = ref(['50% cotton', '30% wool', '20% polyester']);
 
-const title = computed(() => {
-  return brand.value + ' ' + product.value
-})
+  const variants = ref([
+    { id: 2234, color: 'green', image: socksGreenImage, quantity: 50 },
+    { id: 2235, color: 'blue', image: socksBlueImage, quantity: 0 },
+  ]);
 
-const image = computed(() => {
-  return variants.value[selectedVariant.value].image
-})
+  const reviews = ref([]);
 
-const inStock = computed(() => {
-  return variants.value[selectedVariant.value].quantity > 0
-})
+  const title = computed(() => {
+    return brand.value + ' ' + product.value;
+  });
 
-const shipping = computed(() => {
-  if (props.premium) {
-    return 'Free'
-  }
-  else {
-    return 2.99
-  }
-})
+  const image = computed(() => {
+    return variants.value[selectedVariant.value].image;
+  });
 
-const addToCart = () => {
-  emit('add-to-cart', variants.value[selectedVariant.value].id)
-}
+  const inStock = computed(() => {
+    return variants.value[selectedVariant.value].quantity > 0;
+  });
 
-const updateVariant = (index) => {
-  selectedVariant.value = index
-}
+  const shipping = computed(() => {
+    if (props.premium) {
+      return 'Free';
+    } else {
+      return 2.99;
+    }
+  });
+
+  const addToCart = () => {
+    emit('add-to-cart', variants.value[selectedVariant.value].id);
+  };
+
+  const updateVariant = (index) => {
+    selectedVariant.value = index;
+  };
+
+  const addReview = (review) => {
+    reviews.value.push[review];
+    reviews.forEach((value) => {
+      console.log(value);
+    });
+  };
 </script>
 
 <template>
   <div class="product-display">
     <div class="product-container">
-      <div class="product-image">    
-        <img v-bind:src="image">
+      <div class="product-image">
+        <img v-bind:src="image" />
       </div>
       <div class="product-info">
         <h1>{{ title }}</h1>
@@ -68,23 +78,20 @@ const updateVariant = (index) => {
         <ul>
           <li v-for="detail in details">{{ detail }}</li>
         </ul>
-        <div 
-          v-for="(variant, index) in variants" 
+        <div
+          v-for="(variant, index) in variants"
           :key="variant.id"
           @mouseover="updateVariant(index)"
           class="color-circle"
           :style="{ backgroundColor: variant.color }"
         >
         </div>
-        <button
-          class="button" 
-          :class="{ disabledButton: !inStock }"
-          :disabled="!inStock"
-          v-on:click="addToCart"
-        >
+        <button class="button" :class="{ disabledButton: !inStock }" :disabled="!inStock" v-on:click="addToCart">
           Add to cart
         </button>
       </div>
     </div>
+    <ReviewList :reviews="reviews"></ReviewList>
+    <ReviewForm @review-submitted="addReview"></ReviewForm>
   </div>
 </template>
--- src/components/ReviewForm.vue
+++ src/components/ReviewForm.vue
@@ -1,10 +1,35 @@
+<script setup>
+  import { ref, reactive } from 'vue';
+
+  const emit = defineEmits(['review-submitted']);
+
+  const review = reactive({
+    name: '',
+    content: '',
+    rating: null,
+  });
+
+  const onSubmit = () => {
+    const productReview = {
+      name: review.name,
+      content: review.content,
+      rating: review.rating,
+    };
+    emit('review-submitted', productReview);
+
+    review.name = 'kk';
+    review.content = '';
+    review.rating = null;
+  };
+</script>
+
 <template>
-  <form class="review-form">
+  <form class="review-form" @submit.prevent="onSubmit">
     <h3>Leave a review</h3>
     <label for="name">Name:</label>
-    <input id="name" v-model="review.name">
+    <input id="name" v-model="review.name" />
 
-    <label for="review">Review:</label>      
+    <label for="review">Review:</label>
     <textarea id="review" v-model="review.content"></textarea>
 
     <label for="rating">Rating:</label>
@@ -16,6 +41,6 @@
       <option>1</option>
     </select>
 
-    <input class="button" type="submit" value="Submit">
+    <input class="button" type="submit" value="Submit" />
   </form>
 </template>
